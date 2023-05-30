9f39356159cef0a62dec68fc8d048088f8ddc166
Antonio <antoniocn94@hotmail.com>
2023-05-30 23:16:45 +0200

    Optimización

--- server/clases/server.ts
+++ server/clases/server.ts
@@ -7,77 +7,63 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// export class Server {
+//     public app: express.Application;
+    
+//     public port: number = parseInt(process.env.PORT || '3300');
+
+//     public httpServer: HttpServer;
+    
+//     public io: SocketIOServer;
+    
+//     constructor() {
+//         this.app = express();
+
+//         this.httpServer = createServer(this.app);
+
+//     }
+
+//     start(funcion: () => void) {
+//         this.app.listen(this.port, funcion);
+//         // this.httpServer.listen(this.port, funcion);
+
+//         this.io = new SocketIOServer(this.httpServer, {
+//             cors: {
+//                 origin: '*',
+//                 methods: [
+//                     'GET', 'POST'
+//                 ]
+//             }
+//         });
+
+//         configSocket(this.io);
+//     }
+
+// }
+
+// SOCKET
+
 export class Server {
     public app: express.Application;
-    
     public port: number = parseInt(process.env.PORT || '3300');
-
     public httpServer: HttpServer;
-    
     public io: SocketIOServer;
     
     constructor() {
         this.app = express();
-
         this.httpServer = createServer(this.app);
-
-    }
-
-    start(funcion: () => void) {
-        this.app.listen(this.port, funcion);
-        // this.httpServer.listen(this.port, funcion);
-
         this.io = new SocketIOServer(this.httpServer, {
             cors: {
                 origin: '*',
-                methods: [
-                    'GET', 'POST'
-                ]
+                methods: ['GET', 'POST']
             }
         });
 
         configSocket(this.io);
     }
 
+    start(funcion: () => void) {
+        this.httpServer.listen(this.port, funcion);
+    }
 }
 
-// SOCKET
-
-// import express, { Application } from "express";
-// import { createServer, Server as HttpServer } from "http";
-// import { Server as SocketIOServer } from "socket.io";
-// import { configSocket } from "../connections/socket";
-
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// export class Server {
-//   public app: express.Application;
-//   public port_app: number = parseInt(process.env.PORT || '3300');
-//   public port_http: number = parseInt(process.env.PORT || '4000');
-//   public httpServer: HttpServer;
-//   public io: SocketIOServer;
-
-//   constructor() {
-//     this.app = express();
-//     this.httpServer = createServer(this.app);
-//   }
-
-//   start(funcion: () => void) {
-//     this.app.listen(this.port_app, funcion);
-//   }
-
-//   startSocket(funcion: () => void) {
-//     this.httpServer.listen(this.port_http, funcion);
-
-//     this.io = new SocketIOServer(this.httpServer, {
-//       cors: {
-//         origin: '*',
-//         methods: ['GET', 'POST']
-//       }
-//     });
-
-//     configSocket(this.io);
-//   }
-// }
--- server/dist/clases/server.js
+++ server/dist/clases/server.js
@@ -10,56 +10,46 @@ const socket_io_1 = require("socket.io");
 const socket_1 = require("../connections/socket");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+// export class Server {
+//     public app: express.Application;
+//     public port: number = parseInt(process.env.PORT || '3300');
+//     public httpServer: HttpServer;
+//     public io: SocketIOServer;
+//     constructor() {
+//         this.app = express();
+//         this.httpServer = createServer(this.app);
+//     }
+//     start(funcion: () => void) {
+//         this.app.listen(this.port, funcion);
+//         // this.httpServer.listen(this.port, funcion);
+//         this.io = new SocketIOServer(this.httpServer, {
+//             cors: {
+//                 origin: '*',
+//                 methods: [
+//                     'GET', 'POST'
+//                 ]
+//             }
+//         });
+//         configSocket(this.io);
+//     }
+// }
+// SOCKET
 class Server {
     constructor() {
         this.port = parseInt(process.env.PORT || '3300');
         this.app = (0, express_1.default)();
         this.httpServer = (0, http_1.createServer)(this.app);
-    }
-    start(funcion) {
-        this.app.listen(this.port, funcion);
-        // this.httpServer.listen(this.port, funcion);
         this.io = new socket_io_1.Server(this.httpServer, {
             cors: {
                 origin: '*',
-                methods: [
-                    'GET', 'POST'
-                ]
+                methods: ['GET', 'POST']
             }
         });
         (0, socket_1.configSocket)(this.io);
     }
+    start(funcion) {
+        this.httpServer.listen(this.port, funcion);
+    }
 }
 exports.Server = Server;
-// SOCKET
-// import express, { Application } from "express";
-// import { createServer, Server as HttpServer } from "http";
-// import { Server as SocketIOServer } from "socket.io";
-// import { configSocket } from "../connections/socket";
-// import dotenv from 'dotenv';
-// dotenv.config();
-// export class Server {
-//   public app: express.Application;
-//   public port_app: number = parseInt(process.env.PORT || '3300');
-//   public port_http: number = parseInt(process.env.PORT || '4000');
-//   public httpServer: HttpServer;
-//   public io: SocketIOServer;
-//   constructor() {
-//     this.app = express();
-//     this.httpServer = createServer(this.app);
-//   }
-//   start(funcion: () => void) {
-//     this.app.listen(this.port_app, funcion);
-//   }
-//   startSocket(funcion: () => void) {
-//     this.httpServer.listen(this.port_http, funcion);
-//     this.io = new SocketIOServer(this.httpServer, {
-//       cors: {
-//         origin: '*',
-//         methods: ['GET', 'POST']
-//       }
-//     });
-//     configSocket(this.io);
-//   }
-// }
 //# sourceMappingURL=server.js.map
